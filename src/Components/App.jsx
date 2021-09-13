7f8ac736ba9fe10937d27a99c5ff7e9ef6b310de
A-Kija <42836152+A-Kija@users.noreply.github.com>
2021-09-13 15:34:44 +0300

    mygtukas

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,42 @@
-import Mygtukas from './Mygtukas';
+import React from 'react';
 
-const data = [['Spausk', 1], ['Spaudinėk', 10], ['Klikink', 100]];
+import ChangeColorButton from './ChangeColorButton';
 
-function App() {
-    return (<>
-        {data.map((b, i) => <Mygtukas key={i} tekstas={b[0]} amount={b[1]} />)}
-    </>);
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {bg: 'palegreen'};
     }
+
+    changeColor = () => {
+        // this.setState({
+        //     bg: 'orangered',
+        // });
+        this.setState(state => {
+
+            let color;
+            if (state.bg == 'palegreen') {
+                color = 'orangered';
+            }
+            else if (state.bg == 'orangered') {
+                color = 'palegreen';
+            }
+
+            return(
+                {bg: color}
+                )
+            });
+
+    }
+
+    render() {
+        return (
+            <div className="circle" style={{backgroundColor: this.state.bg}}>
+                <ChangeColorButton clickToChangeColor={this.changeColor}></ChangeColorButton>
+            </div>
+        );
+    }
+}
     
 export default App;
