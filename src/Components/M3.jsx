cb211bd8ece0ab9e5767d48b39643f6657d89209
A-Kija <42836152+A-Kija@users.noreply.github.com>
2021-09-20 11:25:24 +0300

    xxx

---
+++ src/Components/M3.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class M3 extends React.Component {
+
+    constructor(props) {
+      super(props);
+        this.state = {x: 3};
+    }
+
+    do3 = () => {
+        localStorage.setItem('xxx', this.state.x + 3);
+        this.setState((state) => ({x: state.x += 3}));
+    }
+
+    componentDidMount() {
+        const x = localStorage.getItem('xxx');
+        if (null === x) {
+            return;
+        }
+        this.setState({
+            x: parseInt(x)
+        })
+    }
+  
+    render() {
+      return (
+        <button onClick={this.do3}>{this.state.x}X</button>
+      );
+    }
+  }
+  export default M3;
