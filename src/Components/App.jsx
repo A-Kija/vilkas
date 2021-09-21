797c1537b439d329703faaa9f29f001865757c9a
A-Kija <42836152+A-Kija@users.noreply.github.com>
2021-09-21 12:17:12 +0300

    Farm 2

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -7,23 +7,24 @@ function App() {
     const [cowInput, setCowInput] = useState('');
 
     useEffect(() => {
-        console.log('animals changed');
-    }, [animals]);
-
-    useEffect(() => {
-        console.log('cowInput changed');
-    }, [cowInput]);
-
-    useEffect(() => {
-        console.log('something changed');
-    }, [cowInput, animals]);
+        const animalsCopy = JSON.parse(localStorage.getItem('allAnimals'));
+        if (null === animalsCopy) {
+            return;
+        }
+        setAnimals(animalsCopy);
+    }, []);
 
     const addAnimal = (a) => {
-        const animal = {id: getId(), color: cowInput, animal: a};
+        const animal = {
+            id: getId(),
+            color: cowInput,
+            animal: a,
+            farm1: true
+        };
         const animalsCopy = animals.slice();
         animalsCopy.push(animal);
         setAnimals(animalsCopy);
-        // localStorage.setItem('allAnimals', JSON.stringify(animals));
+        localStorage.setItem('allAnimals', JSON.stringify(animalsCopy));
     }
 
     const deleteAnimal = (id) => {
@@ -35,7 +36,7 @@ function App() {
             }
         }
         setAnimals(animalsCopy);
-        // localStorage.setItem('allAnimals', JSON.stringify(animals));
+        localStorage.setItem('allAnimals', JSON.stringify(animalsCopy));
     }
 
     const editAnimal = (id, color) => {
@@ -47,26 +48,37 @@ function App() {
             }
         }
         setAnimals(animalsCopy);
-        // localStorage.setItem('allAnimals', JSON.stringify(animals));
+        localStorage.setItem('allAnimals', JSON.stringify(animalsCopy));
+    }
+
+    const changeFarm = (id) => {
+        const animalsCopy = animals.slice();
+        for (let i=0; i<animalsCopy.length; i++) {
+            if (animalsCopy[i].id == id) {
+                animalsCopy[i].farm1 = !animalsCopy[i].farm1;
+                break;
+            }
+        }
+        setAnimals(animalsCopy);
+        localStorage.setItem('allAnimals', JSON.stringify(animalsCopy));
     }
 
     const cowInputHandler = (e) => {
         setCowInput(e.target.value);
     }
 
-    // componentDidMount() {
-    //     const animals = JSON.parse(localStorage.getItem('allAnimals'));
-    //     if (null === animals) {
-    //         return;
-    //     }
-    //     this.useState({
-    //         animals: animals
-    //     })
-    // }
-
     return (
         <>
-            {animals.map(b => <SmallAnimal key={b.id} delete={deleteAnimal} edit={editAnimal} id={b.id} color={b.color} animal={b.animal} />)}
+            <div className="field">
+                <div className="farm">
+                    <h1>Farm No. 1</h1>
+                    {animals.map(b => <SmallAnimal farm1={b.farm1} farmNamber={1} key={b.id} delete={deleteAnimal} edit={editAnimal} id={b.id} color={b.color} animal={b.animal} change={changeFarm} />)}
+                </div>
+                <div className="farm">
+                    <h1>Farm No. 2</h1>
+                    {animals.map(b => <SmallAnimal farm1={b.farm1} farmNamber={2} key={b.id} delete={deleteAnimal} edit={editAnimal} id={b.id} color={b.color} animal={b.animal} change={changeFarm} />)}
+                </div>
+            </div>
             <div>
                 <input type="text" value={cowInput} onChange={cowInputHandler}/>
                 <button className="input-button" onClick={()=>addAnimal('cow')}>Add Cow</button>
