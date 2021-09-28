aeef9b4e0211e6d5689dc71630df8d1380ca05ef
A-Kija <42836152+A-Kija@users.noreply.github.com>
2021-09-28 11:19:16 +0300

    todos router

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,10 +1,11 @@
 import User from './User';
+import {Link} from 'react-router-dom';
 
 function Todo(props) {
 
     return (
         <div className="todo">
-            <div className='id'>{props.data.id}</div>
+            <Link to={'/'+props.data.id}><div className='id'>{props.data.id}</div></Link>
             <User id={props.data.userId}></User>
             <div className="title">{props.data.title}</div>
             <div className="status" onClick={()=>props.sniuriukas(props.data.id)} style={
@@ -13,5 +14,6 @@ function Todo(props) {
         </div>
     );
 }
+
 
 export default Todo;
